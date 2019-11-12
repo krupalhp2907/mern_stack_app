03b01016baf2ffce67be2606e758d7098c41a8a3
krupalhp2907 <57654720+krupalhp2907@users.noreply.github.com>
2019-11-12 12:34:18 +0530

    first commit

---
+++ MERN-Stack-Application/README.md
@@ -0,0 +1,4 @@
+This project was made using ReactJS, NodeJS, ExpressJS and MongoDB.
+
+To run the app, clone the repo and then type the command "npm start".
+The app will automatically open on localhost:3000.
---
+++ MERN-Stack-Application/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import PostList from './components/PostList';
+import EditPost from './components/EditPost';
+import CreatePost from './components/CreatePost';
+import CreateUser from './components/CreateUser';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Route exact path="/" component={PostList} />
+          <Route path="/edit/:id" component={EditPost} />
+          <Route path="/create-post" component={CreatePost} />
+          <Route path="/create-user" component={CreateUser} />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
